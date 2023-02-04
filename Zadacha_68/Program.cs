a05a52d14d3bdfbedc8e173810c884c996b8be43
VadimSid <vadik-izum@tut.by>
2023-02-04 14:13:59 +0300

    68

---
+++ Zadacha_68/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+ Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29   */
+
+
+Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine() ?? "");
+
+int x = FunccioAccerman(m, n);
+Console.Write($"m = {m} n = {n} --> A(m,n) = {x}");
+
+int FunccioAccerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return FunccioAccerman(m - 1, 1);
+    if (m > 0 && n > 0) return FunccioAccerman(m - 1, FunccioAccerman(m, n - 1));
+    return FunccioAccerman(m, n);
+}
+// Тут был гугл в помощь!))) Хотя и просто всё. 
+// Зато понял, почему стоит избегать рекурсий!
